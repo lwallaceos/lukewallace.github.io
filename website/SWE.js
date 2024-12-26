deac62009701a9fa153daca89862029d4219a210
Luke Wallace <lwallace756@students.ccsnh.edu>
2024-12-26 08:34:10 -0500

    Website File

---
+++ website/SWE.js
@@ -0,0 +1,17 @@
+function locationURL(page) {
+     if (page === "Projects") {
+         location.href = "PP.html";
+     } else if (page === "Experience") {
+         location.href = "elp.html";
+     } else if (page === "Education") {
+         location.href = "ee.html";
+     } else if (page === "Contact") {
+         location.href = "cc.html";
+     }
+}
+ 
+ 
+
+
+
+
